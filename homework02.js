eb494caead65f0e6e3c3e29711a63224d03643c2
zachary-o <104291766+zachary-o@users.noreply.github.com>
2022-10-15 23:51:12 +0300

    upload homework02

---
+++ homework02.js
@@ -0,0 +1,10 @@
+
+const numOne = prompt("Enter the first number");
+
+const numTwo = prompt("Enter the second number");
+
+const total = parseInt(numOne) + parseInt(numTwo);
+
+alert(total);
+
+typeof total;
