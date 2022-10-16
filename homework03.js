2f3b96792beb016d813deaddcd43b120c2d5721a
zachary-o <104291766+zachary-o@users.noreply.github.com>
2022-10-17 00:12:54 +0300

    use console.log instead of alert

--- homework03.js
+++ homework03.js
@@ -1,10 +1,7 @@
-
 const numOne = prompt("Enter the first number");
 
 const numTwo = prompt("Enter the second number");
 
 const total = parseInt(numOne) + parseInt(numTwo);
 
-alert(total);
-
-typeof total;
+console.log(typeof total);
