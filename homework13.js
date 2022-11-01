cc278ec1b4610498b27886587e63ca5d5b95147c
zachary-o <104291766+zachary-o@users.noreply.github.com>
2022-11-01 20:53:22 +0200

    modified

--- homework13.js
+++ homework13.js
@@ -3,6 +3,7 @@ let arr = ["HTML", "JavaScript", "CSS"];
 const copySort = (arr) => arr.slice().sort();
 copySort(arr);
 
+const sorted = copySort(arr);
 
 alert(arr);
-alert(copyArr);
+alert(sorted);
